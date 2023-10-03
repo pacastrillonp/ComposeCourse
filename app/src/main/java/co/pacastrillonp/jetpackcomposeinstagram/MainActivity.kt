6cf3b142f00977d9bad01151964a2afa04b073cd
Pablo Castrillon <pactres@gmail.com>
2023-10-03 13:10:34 -0500

    navigation

--- app/src/main/java/co/pacastrillonp/jetpackcomposeinstagram/MainActivity.kt
+++ app/src/main/java/co/pacastrillonp/jetpackcomposeinstagram/MainActivity.kt
@@ -6,11 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import co.pacastrillonp.jetpackcomposeinstagram.model.Routes
 import co.pacastrillonp.jetpackcomposeinstagram.ui.LoginScreen
+import co.pacastrillonp.jetpackcomposeinstagram.ui.Screen1
+import co.pacastrillonp.jetpackcomposeinstagram.ui.Screen2
+import co.pacastrillonp.jetpackcomposeinstagram.ui.Screen3
+import co.pacastrillonp.jetpackcomposeinstagram.ui.Screen4
+import co.pacastrillonp.jetpackcomposeinstagram.ui.Screen5
 import co.pacastrillonp.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +28,37 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeInstagramTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    LoginScreen()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Routes.Screen1.route
+                    ) {
+                        composable(Routes.Screen1.route) { Screen1(navigationController) }
+                        composable(Routes.Screen2.route) { Screen2(navigationController) }
+                        composable(Routes.Screen3.route) { Screen3(navigationController) }
+                        composable(Routes.Screen4.route, arguments = listOf(navArgument("age") {
+                            type = NavType.IntType
+                        })) { backStackEntry ->
+                            Screen4(
+                                navigationController,
+                                backStackEntry.arguments?.getInt("age") ?: 0
+                            )
+                        }
+                        composable(
+                            Routes.Screen5.route, arguments = listOf(
+                                navArgument("name") { defaultValue = "pepe" }
+                            )
+                        ) { backStackEntry ->
+                            Screen5(
+                                navigationController,
+                                backStackEntry.arguments?.getString("name")
+                            )
+                        }
+                    }
                 }
             }
         }
